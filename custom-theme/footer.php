11d37e2501e9d31f250c5f40a954df26eec4e866
Kamaljit Aulakh <98kamaljitkaur@gmail.com>
2024-10-14 16:48:03 -0400

    edit: footer text style

--- custom-theme/footer.php
+++ custom-theme/footer.php
@@ -3,10 +3,11 @@
 <div class="container mt-4 py-4">
     <div class="row">
         <div class="col-md-12">
-            <p class="about-header">About the Data Finder Solution</p>
+            <h4 class="about-header">About the Data Finder Solution</h4>
         </div>
         <div class="col-md-12">
-            <p class="about-description">This tool was developed taking inspiration from Cornell University Research
+            <p class="about-description font-size-16px-md">This tool was developed taking inspiration from Cornell
+                University Research
                 Data Management Service
                 Group and Cornell Information Technologies Custom Development Group (2018). Finder Module. Drupal 8/9.
                 Available
