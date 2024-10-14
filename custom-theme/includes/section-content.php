288854bc7fecc5df8ba2af2a7b03ced56cf904af
Kamaljit Aulakh <98kamaljitkaur@gmail.com>
2024-10-13 21:28:03 -0400

    add: css for content description

--- custom-theme/includes/section-content.php
+++ custom-theme/includes/section-content.php
@@ -1,12 +1,11 @@
 <div class="row info-container py-4">
     <div class="col-md-12">
         <h2 class="info-container-heading border-bottom pb-2 mb-3">Data Storage Finder</h2>
-        <p class="lead text-muted mb-4">Evaluate options for data storage at Cornell</p>
+        <p class="lead text-muted mb-4">Evaluate options for data storage at Syracuse University</p>
 
         <div class="d-flex flex-column gap-1 info-container-details">
             <div class="d-flex align-items-start">
-                <div
-                    class="bg-white rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
+                <div class="rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
                     <i class="fa fa-certificate"></i>
                 </div>
                 <p class="mb-0">All services presented on this finder tool are vetted and supported by Syracuse
@@ -14,15 +13,14 @@
             </div>
             <!-- <div class="d-flex align-items-start">
                     <div
-                        class="bg-white rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
+                        class="rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
                         <i class="fa fa-bullseye"></i>
                     </div>
                     <p class="mb-0">To explore data options available to Weill Cornell Medicine Cornellians please visit
                         the <a href="#" class="info-container-links">WCMC storage wizard</a>.</p>
                 </div> -->
             <div class="d-flex align-items-start">
-                <div
-                    class="bg-white rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
+                <div class="rounded-circle me-1 d-flex align-items-center justify-content-center info-container-icons">
                     <i class="fa fa-comments"></i>
                 </div>
                 <p class="mb-0">We welcome <a href="#" class="info-container-links">feedback</a> on this tool.</p>
