f0e8c9071eca8a3dac2fa5842382d95c1514b06b
Kamaljit Aulakh <98kamaljitkaur@gmail.com>
2024-12-15 16:53:14 -0500

    replacing footer with close button at the top

--- templates/modal-template.php
+++ templates/modal-template.php
@@ -3,18 +3,15 @@
     aria-hidden='false'>
     <div class="modal-container">
         <div class="modal-content">
+            <div class="modal-header">
+                <i id="closeButton" class="icon-times"></i>
+            </div>
             <div class="modal-body">
                 <?php echo do_shortcode('[forminator_form id="330"]'); ?>
             </div>
             <!-- Confirmation Message -->
             <div id="confirmationMessage" style="display: none;">
                 <p>Thank you for your feedback!</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" id="closeButton" class="modal-close-button questions-button-secondary"
-                    data-bs-dismiss="modal">Close</button>
-                <button type="button" id="submitButton" class="modal-feedback-button questions-button-primary">Submit
-                    Feedback</button>
             </div>
         </div>
     </div>
